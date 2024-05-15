002e498375852bb2ef1af5bc6a75a508e59abf66
artskyai <info@artsky.io>
2024-05-15 22:35:39 +0500

    change

--- app/dashboard/components/RiskReward.jsx
+++ app/dashboard/components/RiskReward.jsx
@@ -4,7 +4,7 @@ const RiskReward = () => {
   return (
     <div className="">
       <div className="flex justify-center items-center h-[100vh]">
-        <h2 className="text-[40px] font-bold">We're on it, stay tuned.</h2>
+        <h2 className="text-[40px] font-bold">We&apos;re on it, stay tuned.</h2>
       </div>
     </div>
   );
